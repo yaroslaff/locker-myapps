2fc0ff40b762b2a3970f6ae303af89fd4b40a145
Yaroslav Polyakov <yaroslaff@gmail.com>
2022-06-07 23:52:37 +0700

    default values + debug

--- startme/mods/myapps/__init__.py
+++ startme/mods/myapps/__init__.py
@@ -10,11 +10,10 @@ class StartMeMyApps(StartMeThread):
         verbose=True
         insecure=False
         hook = os.getenv('MYAPPS_HOOK', 'redis').split(' ')
-        event = os.getenv('MYAPPS_EVENT')
+        event = os.getenv('MYAPPS_EVENT', 'update')
         message = os.getenv('MYAPPS_MESSAGE')
         room = os.getenv('MYAPPS_ROOM', 'myapps-{u}')
 
-        print("startmemyapps!")
-        print("hook", hook)
+        print(f"{self}: {host=} {hook=} {event=} {message=} {room=}")
         loop(host = host, key = key, verbose=verbose, insecure=insecure, 
             one=False, hook=hook, event=event, message=message, room=room)
