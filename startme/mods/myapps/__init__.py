88763935ef7eac1d563b48897aae240619961e27
Yaroslav Polyakov <yaroslaff@gmail.com>
2022-06-13 17:15:23 +0700

    startme class changed

--- startme/mods/myapps/__init__.py
+++ startme/mods/myapps/__init__.py
@@ -1,19 +1,38 @@
-from startme import StartMeThread
-from myapps import loop
+from startme import StartMe
+from myapps import run
 import os
+from flask_socketio import SocketIO
 
 
-class StartMeMyApps(StartMeThread):
-    def code(self):
-        host = os.getenv('MYAPPS_HOST')
-        key = os.getenv('MYAPPS_KEY')
-        verbose=True
-        insecure=False
-        hook = os.getenv('MYAPPS_HOOK', 'redis').split(' ')
-        event = os.getenv('MYAPPS_EVENT', 'update')
-        message = os.getenv('MYAPPS_MESSAGE')
-        room = os.getenv('MYAPPS_ROOM', 'myapps-{u}')
+class StartMeMyApps(StartMe):
+    def __init__(self):
+        self.host = os.getenv('MYAPPS_HOST')
+        self.key = os.getenv('MYAPPS_KEY')
+        self.verbose=True
+        self.insecure=False
 
-        print(f"{self}: {host=} {hook=} {event=} {message=} {room=}")
-        loop(host = host, key = key, verbose=verbose, insecure=insecure, 
-            one=False, hook=hook, event=event, message=message, room=room)
+        self.event = os.getenv('MYAPPS_EVENT', 'update')
+        self.message = os.getenv('MYAPPS_MESSAGE')
+        self.room = os.getenv('MYAPPS_ROOM', 'myapps-{u}')
+
+        self.period = 60
+        self.socketio = SocketIO(message_queue="redis://")
+
+        #print(f"{self}: {self.host=} {self.hook=} {self.event=} {self.message=} {self.room=}")
+        #loop(host = host, key = key, verbose=verbose, insecure=insecure, 
+        #    one=False, hook=hook, event=event, message=message, room=room)
+
+    def on_start(self):
+        print(self, "on_start()")
+
+    def on_schedule(self):
+        print(self, "on_schedule()")
+        self.run()
+    
+    def run(self):
+        while True:
+            userlist = run()
+            for u in userlist:
+                roomname = self.room.format(u=u)
+                print(f"notify user {u} in {roomname}")
+                self.socketio.emit(self.event, self.message, room=roomname)
